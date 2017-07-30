2e2bd8a9565a7a52581c11f97f74e139599dbb77
Richard Mehlinger <rmehlinger@gmail.com>
2017-07-30 14:58:36 -0700

    Cleanup decaffeinate detritus; add eslint

--- src/main.js
+++ src/main.js
@@ -1,34 +1,43 @@
-import $ from 'jquery';
-import 'es5-shim';
-import 'es6-shim';
-import _ from 'underscore';
-import * as rx from 'bobtail-rx';
+import $ from "jquery";
+import "es5-shim";
+import "es6-shim";
+import _ from "underscore";
+import * as rx from "bobtail-rx";
 
-let mktag, radio;
-let prop, tag;
+let mktag;
 $.fn.rx = function(prop) {
-  let map = this.data('rx-map');
-  if ((map == null)) { this.data('rx-map', (map = Object.create(null))); }
-  if (prop in map) { return map[prop]; }
-  return map[prop] =
-    (() => { switch (prop) {
-      case 'focused':
-        let focused = rx.cell(this.is(':focus'));
-        this.focus(() => focused.set(true));
-        this.blur(() => focused.set(false));
-        return focused;
-      case 'val':
-        let val = rx.cell(this.val());
-        this.change(() => val.set(this.val()));
-        this.on('input', () => val.set(this.val()));
-        return val;
-      case 'checked':
-        let checked = rx.cell(this.is(':checked'));
-        this.change(() => checked.set(this.is(':checked')));
-        return checked;
-      default:
-        throw new Error('Unknown reactive property type');
-    } })();
+  let map = this.data("rx-map");
+  if ((map == null)) {
+    this.data("rx-map", (map = Object.create(null)));
+  }
+  if (!(prop in map)) {
+    switch (prop) {
+    case "focused": {
+      let focused = rx.cell(this.is(":focus"));
+      this.focus(() => focused.set(true));
+      this.blur(() => focused.set(false));
+      map[prop] = focused;
+      break;
+    }
+    case "val": {
+      let val = rx.cell(this.val());
+      this.change(() => val.set(this.val()));
+      this.on("input", () => val.set(this.val()));
+      map[prop] = val;
+      break;
+    }
+    case "checked": {
+      let checked = rx.cell(this.is(":checked"));
+      this.change(() => checked.set(this.is(":checked")));
+      map[prop] = checked;
+      break;
+    }
+    default: {
+      throw new Error("Unknown reactive property type");
+    }
+    }
+  }
+  return map[prop];
 };
 
 //
@@ -82,20 +91,16 @@ for (let ev of Array.from(DOMEvents)) {
 // http://blog.jquery.com/2011/05/10/jquery-1-6-1-rc-1-released/
 // http://api.jquery.com/prop/
 
-let props = ['async', 'autofocus', 'checked', 'location', 'multiple', 'readOnly',
-  'selected', 'selectedIndex', 'tagName', 'nodeName', 'nodeType',
-  'ownerDocument', 'defaultChecked', 'defaultSelected'];
-let propSet = _.object((() => {
-  let result = [];
-  for (prop of Array.from(props)) {       result.push([prop, null]);
-  }
-  return result;
-})());
+let props = ["async", "autofocus", "checked", "location", "multiple", "readOnly",
+  "selected", "selectedIndex", "tagName", "nodeName", "nodeType",
+  "ownerDocument", "defaultChecked", "defaultSelected"
+];
+let propSet = _.object(props.map(prop => [prop, null]));
 
 let setProp = function(elt, prop, val) {
   if (elt instanceof SVGElement) {
     return elt.setAttribute(prop, val);
-  } else if (prop === 'value') {
+  } else if (prop === "value") {
     return elt.val(val);
   } else if (prop in propSet) {
     return elt.prop(prop, val);
@@ -107,13 +112,12 @@ let setProp = function(elt, prop, val) {
 let setDynProp = function(elt, prop, val, xform) {
   if (xform == null) { xform = _.identity; }
   if (val instanceof rx.ObsCell) {
-    return rx.autoSub(val.onSet, function(...args) {
-      let [o,n] = Array.from(args[0]);
+    return rx.autoSub(val.onSet, function([o, n]) {
       setProp(elt, prop, xform(n));
       if (events.enabled) {
         return events.onElementAttrsChanged.pub({$element: elt, attr: prop});
       }
-  });
+    });
   } else {
     return setProp(elt, prop, xform(val));
   }
@@ -148,60 +152,38 @@ let normalizeTagArgs = function(arg1, arg2) {
   }
 };
 
-let toNodes = contents =>
-  (() => {
-    let result1 = [];
-    for (let child of Array.from(contents)) {
-      if (child != null) {
-        if (_.isString(child) || _.isNumber(child)) {
-          result1.push(document.createTextNode(child));
-        } else if (child instanceof Element || child instanceof SVGElement) {
-          result1.push(child);
-        } else if (child instanceof RawHtml) {
-          let parsed = $(child.html);
-          if (parsed.length !== 1) { throw new Error('RawHtml must wrap a single element'); }
-          result1.push(parsed[0]);
-        } else if (child instanceof $) {
-          if (child.length !== 1) { throw new Error('jQuery object must wrap a single element'); }
-          result1.push(child[0]);
-        } else {
-          throw new Error(`Unknown element type in array: ${child.constructor.name} (must be string, number, Element, RawHtml, or jQuery objects)`);
-        }
+let toNodes = contents => {
+  let result1 = [];
+  for (let child of Array.from(contents)) {
+    if (child != null) {
+      if (_.isString(child) || _.isNumber(child)) {
+        result1.push(document.createTextNode(child));
+      } else if (child instanceof Element || child instanceof SVGElement) {
+        result1.push(child);
+      } else if (child instanceof RawHtml) {
+        let parsed = $(child.html);
+        if (parsed.length !== 1) { throw new Error("RawHtml must wrap a single element"); }
+        result1.push(parsed[0]);
+      } else if (child instanceof $) {
+        if (child.length !== 1) { throw new Error("jQuery object must wrap a single element"); }
+        result1.push(child[0]);
       } else {
-        result1.push(undefined);
+        throw new Error(`Unknown element type in array: ${child.constructor.name} (must be string, number, Element, RawHtml, or jQuery objects)`);
       }
+    } else {
+      result1.push(undefined);
     }
-    return result1;
-  })()
-;
+  }
+  return result1;
+};
 
 let updateContents = function(elt, contents) {
-  let node;
-  if (elt.html) { elt.html(''); }
+  if (elt.html) { elt.html(""); }
   if ((contents == null)) {
     return;
   } else if (_.isArray(contents)) {
     let nodes = toNodes(contents);
     elt.append(nodes);
-    if (false) { // this is super slow
-      let hasWidth = function(node) {
-        try { return ($(node).width() != null) !== 0; }
-        catch (e) { return false; }
-      };
-      let covers = (() => {
-        let result1 = [];
-        for (node of Array.from(nodes != null ? nodes : [])) {
-          if (hasWidth(node)) {
-            let {left, top} = $(node).offset();
-            result1.push($('<div/>').appendTo($('body').first())
-              .addClass('updated-element').offset({top,left})
-              .width($(node).width()).height($(node).height()));
-          }
-        }
-        return result1;
-      })();
-      setTimeout((() => Array.from(covers).map((cover) => $(cover).remove())), 2000);
-    }
     return nodes;
   } else if (_.isString(contents) || _.isNumber(contents) || contents instanceof Element ||
       contents instanceof SVGElement || contents instanceof RawHtml || contents instanceof $) {
@@ -227,7 +209,7 @@ mktag = tag =>
         rx.autoSub(contents.indexed().onChangeCells, function(...args) {
           let [index, removed, added] = Array.from(args[0]);
           elt.contents().slice(index, index + removed.length).remove();
-          let toAdd = toNodes(added.map(function(...args1) { let cell, icell; [cell, icell] = Array.from(args1[0]); return rx.snap(() => cell.get()); }));
+          let toAdd = toNodes(added.map(([cell, icell]) => rx.snap(() => cell.get())));
           if (index === elt.contents().length) {
             elt.append(toAdd);
           } else {
@@ -245,8 +227,7 @@ mktag = tag =>
             let result1 = [];
             for (let [cell, icell] of Array.from(added)) {
               result1.push(((cell, icell) =>
-                rx.autoSub(cell.onSet, rx.skipFirst(function(...args1) {
-                  let [old, val] = Array.from(args1[0]);
+                rx.autoSub(cell.onSet, rx.skipFirst(([old, val]) => {
                   let ival = rx.snap(() => icell.get());
                   toAdd = toNodes([val]);
                   elt.contents().eq(ival).replaceWith(toAdd);
@@ -259,7 +240,7 @@ mktag = tag =>
             }
             return result1;
           })();
-      });
+        });
       } else {
         updateContents(elt, contents);
       }
@@ -278,37 +259,37 @@ mktag = tag =>
 //
 //     "['"+document.body.innerText.match(/<.*?>/g).map(function(x){return x.substring(1, x.length-1);}).join("', '")+"']";
 
-let tags = ['html', 'head', 'title', 'base', 'link', 'meta', 'style', 'script',
-  'noscript', 'body', 'body', 'section', 'nav', 'article', 'aside', 'h1', 'h2',
-  'h3', 'h4', 'h5', 'h6', 'h1', 'h6', 'header', 'footer', 'address', 'main',
-  'main', 'p', 'hr', 'pre', 'blockquote', 'ol', 'ul', 'li', 'dl', 'dt', 'dd',
-  'dd', 'figure', 'figcaption', 'div', 'a', 'em', 'strong', 'small', 's',
-  'cite', 'q', 'dfn', 'abbr', 'data', 'time', 'code', 'var', 'samp', 'kbd',
-  'sub', 'sup', 'i', 'b', 'u', 'mark', 'ruby', 'rt', 'rp', 'bdi', 'bdo',
-  'span', 'br', 'wbr', 'ins', 'del', 'img', 'iframe', 'embed', 'object',
-  'param', 'object', 'video', 'audio', 'source', 'video', 'audio', 'track',
-  'video', 'audio', 'canvas', 'map', 'area', 'area', 'map', 'svg', 'math',
-  'table', 'caption', 'colgroup', 'col', 'tbody', 'thead', 'tfoot', 'tr', 'td',
-  'th', 'form', 'fieldset', 'legend', 'fieldset', 'label', 'input', 'button',
-  'select', 'datalist', 'optgroup', 'option', 'select', 'datalist', 'textarea',
-  'keygen', 'output', 'progress', 'meter', 'details', 'summary', 'details',
-  'menuitem', 'menu'];
+let tags = ["html", "head", "title", "base", "link", "meta", "style", "script",
+  "noscript", "body", "body", "section", "nav", "article", "aside", "h1", "h2",
+  "h3", "h4", "h5", "h6", "h1", "h6", "header", "footer", "address", "main",
+  "main", "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd",
+  "dd", "figure", "figcaption", "div", "a", "em", "strong", "small", "s",
+  "cite", "q", "dfn", "abbr", "data", "time", "code", "var", "samp", "kbd",
+  "sub", "sup", "i", "b", "u", "mark", "ruby", "rt", "rp", "bdi", "bdo",
+  "span", "br", "wbr", "ins", "del", "img", "iframe", "embed", "object",
+  "param", "object", "video", "audio", "source", "video", "audio", "track",
+  "video", "audio", "canvas", "map", "area", "area", "map", "svg", "math",
+  "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr", "td",
+  "th", "form", "fieldset", "legend", "fieldset", "label", "input", "button",
+  "select", "datalist", "optgroup", "option", "select", "datalist", "textarea",
+  "keygen", "output", "progress", "meter", "details", "summary", "details",
+  "menuitem", "menu"];
 
 // From <https://developer.mozilla.org/en-US/docs/Web/SVG/Element>
-let svg_tags = ['a', 'altglyph', 'altglyphdef', 'altglyphitem', 'animate',
-  'animatecolor', 'animatemotion', 'animatetransform', 'circle', 'clippath',
-  'color-profile', 'cursor', 'defs', 'desc', 'ellipse', 'feblend',
-  'fecolormatrix', 'fecomponenttransfer', 'fecomposite', 'feconvolvematrix',
-  'fediffuselighting', 'fedisplacementmap', 'fedistantlight', 'feflood',
-  'fefunca', 'fefuncb', 'fefuncg', 'fefuncr', 'fegaussianblur', 'feimage',
-  'femerge', 'femergenode', 'femorphology', 'feoffset', 'fepointlight',
-  'fespecularlighting', 'fespotlight', 'fetile', 'feturbulence', 'filter',
-  'font', 'font-face', 'font-face-format', 'font-face-name', 'font-face-src',
-  'font-face-uri', 'foreignobject', 'g', 'glyph', 'glyphref', 'hkern', 'image',
-  'line', 'lineargradient', 'marker', 'mask', 'metadata', 'missing-glyph',
-  'mpath', 'path', 'pattern', 'polygon', 'polyline', 'radialgradient', 'rect',
-  'script', 'set', 'stop', 'style', 'svg', 'switch', 'symbol', 'text',
-  'textpath', 'title', 'tref', 'tspan', 'use', 'view', 'vkern'];
+let svg_tags = ["a", "altglyph", "altglyphdef", "altglyphitem", "animate",
+  "animatecolor", "animatemotion", "animatetransform", "circle", "clippath",
+  "color-profile", "cursor", "defs", "desc", "ellipse", "feblend",
+  "fecolormatrix", "fecomponenttransfer", "fecomposite", "feconvolvematrix",
+  "fediffuselighting", "fedisplacementmap", "fedistantlight", "feflood",
+  "fefunca", "fefuncb", "fefuncg", "fefuncr", "fegaussianblur", "feimage",
+  "femerge", "femergenode", "femorphology", "feoffset", "fepointlight",
+  "fespecularlighting", "fespotlight", "fetile", "feturbulence", "filter",
+  "font", "font-face", "font-face-format", "font-face-name", "font-face-src",
+  "font-face-uri", "foreignobject", "g", "glyph", "glyphref", "hkern", "image",
+  "line", "lineargradient", "marker", "mask", "metadata", "missing-glyph",
+  "mpath", "path", "pattern", "polygon", "polyline", "radialgradient", "rect",
+  "script", "set", "stop", "style", "svg", "switch", "symbol", "text",
+  "textpath", "title", "tref", "tspan", "use", "view", "vkern"];
 
 let updateSVGContents = function(elt, contents) {
   while (elt.firstChild) { elt.removeChild(elt.firstChild); }
@@ -318,7 +299,9 @@ let updateSVGContents = function(elt, contents) {
   } else if (_.isString(contents) || contents instanceof SVGElement) {
     return updateSVGContents(elt, [contents]);
   } else {
-    console.error('updateSVGContents', elt, contents);
+    /*eslint-disable*/
+    console.error("updateSVGContents", elt, contents);
+    /*eslint-enable*/
     throw `Must wrap contents ${contents} as array or string`;
   }
 };
@@ -327,7 +310,7 @@ let svg_mktag = tag =>
   function(arg1, arg2) {
     let [attrs, contents] = Array.from(normalizeTagArgs(arg1, arg2));
 
-    let elt = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    let elt = document.createElementNS("http://www.w3.org/2000/svg", tag);
     let object = _.omit(attrs, _.keys(specialAttrs));
     for (let name in object) {
       let value = object[name];
@@ -337,28 +320,17 @@ let svg_mktag = tag =>
     if (contents != null) {
       if (contents instanceof rx.ObsArray) {
         contents.onChange.sub(function(...args) {
-          let node;
           let [index, removed, added] = Array.from(args[0]);
           for (let i = 0, end = removed.length, asc = 0 <= end; asc ? i < end : i > end; asc ? i++ : i--) { elt.removeChild(elt.childNodes[index]); }
           let toAdd = toNodes(added);
           if (index === elt.childNodes.length) {
-            return (() => {
-              let result1 = [];
-              for (node of Array.from(toAdd)) {                   result1.push((elt.appendChild(node)));
-              }
-              return result1;
-            })();
+            return toAdd.map((node) => elt.appendChild(node));
           } else {
-            return (() => {
-              let result2 = [];
-              for (node of Array.from(toAdd)) {                   result2.push((elt.childNodes[index].insertBefore(node)));
-              }
-              return result2;
-            })();
+            return toAdd.map(node => elt.childNodes[index].insertBefore(node));
           }
         });
       } else if (contents instanceof rx.ObsCell) {
-        contents.onSet.sub(function(...args) { let [old, val] = Array.from(args[0]); return updateSVGContents(elt, val); });
+        contents.onSet.sub(([old, val]) => updateSVGContents(elt, val));
       } else {
         updateSVGContents(elt, contents);
       }
@@ -376,26 +348,26 @@ tags = _.object(tags.map(tag => [tag, mktag(tag)]));
 let {input} = tags;
 
 let _input = (type, opts) => input(_.extend({type}, opts));
-input.color = opts => _input('color', opts);
-input.date = opts => _input('date', opts);
-input.datetime = opts => _input('datetime', opts);
-input.datetimeLocal = opts => _input('datetime-local', opts);
-input.email = opts => _input('email', opts);
-input.file = opts => _input('file', opts);
-input.hidden = opts => _input('hidden', opts);
-input.image = opts => _input('image', opts);
-input.month = opts => _input('month', opts);
-input.number = opts => _input('number', opts);
-input.password = opts => _input('password', opts);
-input.range = opts => _input('range', opts);
-input.reset = opts => _input('reset', opts);
-input.search = opts => _input('search', opts);
-input.submit = opts => _input('submit', opts);
-input.tel = opts => _input('tel', opts);
-input.text = opts => _input('text', opts);
-input.time = opts => _input('time', opts);
-input.url = opts => _input('url', opts);
-input.week = opts => _input('week', opts);
+input.color = opts => _input("color", opts);
+input.date = opts => _input("date", opts);
+input.datetime = opts => _input("datetime", opts);
+input.datetimeLocal = opts => _input("datetime-local", opts);
+input.email = opts => _input("email", opts);
+input.file = opts => _input("file", opts);
+input.hidden = opts => _input("hidden", opts);
+input.image = opts => _input("image", opts);
+input.month = opts => _input("month", opts);
+input.number = opts => _input("number", opts);
+input.password = opts => _input("password", opts);
+input.range = opts => _input("range", opts);
+input.reset = opts => _input("reset", opts);
+input.search = opts => _input("search", opts);
+input.submit = opts => _input("submit", opts);
+input.tel = opts => _input("tel", opts);
+input.text = opts => _input("text", opts);
+input.time = opts => _input("time", opts);
+input.url = opts => _input("url", opts);
+input.week = opts => _input("week", opts);
 
 let swapChecked = function($input) {
   /*
@@ -422,13 +394,13 @@ input.checkbox = opts =>
   swapChecked(input(_.extend({type: "checkbox"}, opts)))
 ;
 
-input.radio = (radio = opts => swapChecked(input(_.extend({type: "radio"}, opts))));
+input.radio = (opts => swapChecked(input(_.extend({type: "radio"}, opts))));
 
 svg_tags = _.object(svg_tags.map(svg_tag => [svg_tag, svg_mktag(svg_tag)]));
 
 let rawHtml = html => new RawHtml(html);
-let specialChar = function(code, tag) { if (tag == null) { tag = 'span'; } return rawHtml(`<${tag}>&${code};</${tag}>`); };
-let unicodeChar = function(code, tag) { if (tag == null) { tag = 'span'; } return rawHtml(`<${tag}>\\u${code};</${tag}>`); };
+let specialChar = function(code, tag) { if (tag == null) { tag = "span"; } return rawHtml(`<${tag}>&${code};</${tag}>`); };
+let unicodeChar = function(code, tag) { if (tag == null) { tag = "span"; } return rawHtml(`<${tag}>\\u${code};</${tag}>`); };
 //
 // rxt utilities
 //
@@ -436,32 +408,31 @@ let unicodeChar = function(code, tag) { if (tag == null) { tag = 'span'; } retur
 // a little underscore-string inlining
 let trim = $.trim;
 
-let dasherize = str=> trim(str).replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
+let dasherize = str=> trim(str).replace(/([A-Z])/g, "-$1").replace(/[-_\s]+/g, "-").toLowerCase();
 
 specialAttrs.style = function(elt, value) {
   let isCell = value instanceof rx.ObsCell;
-  return rx.autoSub(rx.cast(value).onSet, function(...args) {
-    let [o,n] = Array.from(args[0]);
+  return rx.autoSub(rx.cast(value).onSet, ([o, n]) => {
     if ((n == null) || _.isString(n)) {
-      setProp(elt, 'style', n);
+      setProp(elt, "style", n);
     } else {
-      elt.removeAttr('style').css(n);
+      elt.removeAttr("style").css(n);
     }
     if (isCell && events.enabled) {
       return events.onElementAttrsChanged.pub({$element: elt, attr: "style"});
     }
-});
+  });
 };
 
-let smushClasses = xs => _(xs).chain().flatten().compact().value().join(' ').replace(/\s+/, ' ').trim();
+let smushClasses = xs => _(xs).chain().flatten().compact().value().join(" ").replace(/\s+/, " ").trim();
 
 specialAttrs.class = (elt, value) =>
-  setDynProp(elt, 'class', value, function(val) {
+  setDynProp(elt, "class", value, function(val) {
     if (_.isString(val)) { return val; } else { return smushClasses(val); }
   })
 ;
 
-export * from 'bobtail-rx';
+export * from "bobtail-rx";
 export let rxt = {
   events, RawHtml, specialAttrs, mktag, svg_mktag, tags, svg_tags, rawHtml, specialChar, unicodeChar,
   trim, dasherize, smushClasses
